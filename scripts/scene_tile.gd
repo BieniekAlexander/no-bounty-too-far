83a049415e20c1b441f6d0c9821162aa591a780e
BieniekAlexander <bieniekalexander@gmail.com>
2025-09-19 13:24:12 -0400

    WIP inventory system

--- scripts/scene_tile.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var dust: PackedScene = preload("res://scenes/dust_tile.tscn")
-
-func on_queue_free():
-	var new = dust.instantiate()
-	get_parent().add_child(new)
-	new.global_position = global_position
-	queue_free()
